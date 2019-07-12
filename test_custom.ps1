d70cd01ce9f53ccc14f02474e64c2485a0bf2122
Vitaly Sibichenkov <vit@sitecore.com>
2019-07-12 12:15:17 +0300

    test

--- test_custom.ps1
+++ test_custom.ps1
@@ -9,7 +9,8 @@ Get-ChildItem -Path "D:\" | Out-File $logfile
 
 # download soft
 Invoke-WebRequest -Uri https://notepad-plus-plus.org/repository/7.x/7.7.1/npp.7.7.1.Installer.x64.exe -OutFile D:\soft\note.exe -UseBasicParsing
-Invoke-WebRequest -Uri http://az764295.vo.msecnd.net/stable/c7d83e57cd18f18026a8162d042843bda1bcf21f/VSCodeSetup-x64-1.35.1.exe -OutFile  D:\soft\vscode.exe -UseBasicParsing
+#Invoke-WebRequest -Uri http://az764295.vo.msecnd.net/stable/c7d83e57cd18f18026a8162d042843bda1bcf21f/VSCodeSetup-x64-1.35.1.exe -OutFile  D:\soft\vscode.exe -UseBasicParsing
+Invoke-WebRequest -Uri https://vscode-update.azurewebsites.net/latest/win32-x64/stable -OutFile  D:\soft\vscode.exe -UseBasicParsing
 Invoke-WebRequest -Uri https://github.com/git-for-windows/git/releases/download/v2.22.0.windows.1/Git-2.22.0-64-bit.exe -OutFile D:\soft\git.exe -UseBasicParsing
 
 # install soft
