ffa1f6a7310dbf7a177f58491ae99ec7db31e436
Vitaly Sibichenkov <sibich.v@gmail.com>
2019-10-22 23:56:34 +0300

    changes

--- test_custom.ps1
+++ test_custom.ps1
@@ -9,16 +9,19 @@ write-output (get-date -Format yyyy-MM-dd-hh-mm-ss)"Folders were created:" | Out
 Get-ChildItem -Path "D:\" -directory | Out-File -FilePath $logfile -Append
 
 # download soft
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest -Uri http://download.notepad-plus-plus.org/repository/7.x/7.8/npp.7.8.Installer.x64.exe -OutFile D:\soft\note.exe -UseBasicParsing
 Invoke-WebRequest -Uri http://az764295.vo.msecnd.net/stable/c7d83e57cd18f18026a8162d042843bda1bcf21f/VSCodeSetup-x64-1.35.1.exe -OutFile  D:\soft\vscode.exe -UseBasicParsing
 Invoke-WebRequest -Uri https://github.com/git-for-windows/git/releases/download/v2.23.0.windows.1/Git-2.23.0-64-bit.exe -OutFile D:\soft\git.exe -UseBasicParsing
 Invoke-WebRequest -Uri https://download.visualstudio.microsoft.com/download/pr/53f250a1-318f-4350-8bda-3c6e49f40e76/e8cbbd98b08edd6222125268166cfc43/dotnet-sdk-3.0.100-win-x64.exe -OutFile D:\soft\dotnet.exe -UseBasicParsing
-
+Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile D:\soft\AzureCLI.msi -UseBasicParsing
+Invoke-WebRequest -Uri https://github.com/docker/compose/releases/download/1.24.1/docker-compose-Windows-x86_64.exe -UseBasicParsing -OutFile $Env:ProgramFiles\Docker\docker-compose.exe
 # install soft
 & D:\soft\note.exe /S
 & D:\soft\git.exe /VERYSILENT
 & D:\soft\vscode.exe /VERYSILENT /NORESTART /MERGETASKS=!runcode
 & D:\soft\dotnet.exe /QUIET
+Start-Process msiexec.exe -Wait -ArgumentList '/I D:\soft\AzureCLI.msi /quiet'
 start-sleep -Seconds 240
 write-output (get-date -Format yyyy-MM-dd-hh-mm-ss)"Software was installed:" | Out-File -FilePath $logfile -Append
 Get-ChildItem 'C:\Program Files\Notepad++\' -Name notepad++.exe | Out-File $logfile -Append
@@ -82,11 +85,6 @@ $disks = Get-Disk | Where-Object partitionstyle -eq 'raw' | Sort-Object number
 
 #start docker
 restart-Service Docker
-
-Get-ChildItem -Path "C:\Programdata\docker\" | Out-File $logfile -Append
-start-sleep -Seconds 120
-Get-ChildItem -Path "C:\Programdata\docker\" | Out-File $logfile -Append
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/sibich/publicscripts/master/daemon.json -OutFile C:\Programdata\docker\config\daemon.json -UseBasicParsing
 
 
 # install updates
