ea20d5c1c8f6f6baa82a7ee1743403bb9d71d572
Vitaly Sibichenkov <sibich.v@gmail.com>
2019-12-15 11:28:50 +0200

    some changes

--- kube_path.ps1
+++ kube_path.ps1
@@ -1,3 +1,7 @@
 ﻿$PATH = [Environment]::GetEnvironmentVariable("PATH")
 $kube_path = 'C:\Users\vitaly\.azure-kubectl'
 [Environment]::SetEnvironmentVariable("PATH", "$PATH;$kube_path", "Machine")
+
+$PATH = [Environment]::GetEnvironmentVariable("PATH")
+$helm_path = 'C:\Users\vitaly\helm\windows-amd64'
+[Environment]::SetEnvironmentVariable("PATH", "$PATH;$helm_path", "Machine")
