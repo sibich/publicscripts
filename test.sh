3938cddc5b77c903a633099688b22e893f3ffea6
Vitaly Sibichenkov <sibich.v@gmail.com>
2020-01-06 19:46:33 +0200

    some changes

--- test.sh
+++ test.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
 user=$(whoami)
-echo "Hello $user from ARM" > /home/vitaly/test_`date +%d-%m-%Y"_"%H_%M_%S`.txt
+folder=$(pwd)
+echo "Hello $user from ARM in $folder" > /home/vitaly/test_`date +%d-%m-%Y"_"%H_%M_%S`.txt
+#prerequisites
+apt-get update
+apt-get upgrade -y
+apt-get install -y apt-transport-https curl ca-certificates gnupg2 software-properties-common
+
+swapoff -a
+
+#install docker
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+apt-get -y update
+apt-cache policy docker-ce
+apt-get install -y docker-ce docker-ce-cli containerd.io
+dockerver=$(docker --version)
+echo "Docker version is $dockerver" > /home/vitaly/setup.log
 
