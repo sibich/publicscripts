7fcb8746b783ae0911d2fa297c79fb77193a9d95
Vitaly Sibichenkov <vit@sitecore.com>
2020-01-09 11:50:56 +0200

    changes

--- test.sh
+++ test.sh
@@ -2,13 +2,17 @@
 
 user=$(whoami)
 folder=$(pwd)
-echo "Hello $user from ARM in $folder" > /home/vitaly/test_`date +%d-%m-%Y"_"%H_%M_%S`.txt
+date=$(date +%d-%m-%Y" "%H:%M:%S)
+echo "Script started: $date" > /home/vitaly/setup.log
+echo "Current user is $user, working in $folder folder" >> /home/vitaly/setup.log
 #prerequisites
 apt-get update
 apt-get upgrade -y
 apt-get install -y apt-transport-https curl ca-certificates gnupg2 software-properties-common
 
+#disabling swap
 swapoff -a
+sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 
 #install docker
 curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
@@ -18,15 +22,15 @@ apt-get -y update
 apt-cache policy docker-ce
 apt-get install -y docker-ce docker-ce-cli containerd.io
 dockerver=$(docker --version)
-echo "Docker version is $dockerver" > /home/vitaly/setup.log
+echo "Docker version is $dockerver" >> /home/vitaly/setup.log
 
 #install k8s
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sleep 10s
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list
 apt-get update
-apt-get install -y kubeadm kubelet
-apt-mark hold kubelet kubeadm
+apt-get install -y kubeadm kubelet kubectl kubernetes-cni
+apt-mark hold kubelet kubeadm kubectl
 
 kubeadmver=$(kubeadm version)
 echo "Kubeadm version is $kubeadmver" >> /home/vitaly/setup.log
