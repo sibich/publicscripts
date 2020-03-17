5cf7be72ce0e605a04e5090a14b1bd583ba2d59e
Vitaly Sibichenkov <vit@sitecore.com>
2020-03-17 11:23:31 +0200

    some changes

--- webserver.sh
+++ webserver.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#modified 17.01
+#modified 17.03
 user=$(whoami)
 folder=$(pwd)
 date=$(date +%d-%m-%Y" "%H:%M:%S)
@@ -12,15 +12,14 @@ apt-get install -y curl lsb-release apt-transport-https ca-certificates
 
 curlver=$(curl --version)
 echo "Curl version is $curlver" >> /home/vitaly/webserver_setup.log
-sudover=$(sudo --version)
-echo "Sudo version is $sudover" >> /home/vitaly/webserver_setup.log
 
 #install nginx:
 apt-get install -y gnupg gnupg1 gnupg2
 wget http://nginx.org/keys/nginx_signing.key
 apt-key add nginx_signing.key
-echo "deb http://nginx.org/packages/debian/ buster nginx" | tee /etc/apt/sources.list.d/nginx.list
-echo "deb-src http://nginx.org/packages/debian/ buster nginx" | tee /etc/apt/sources.list.d/nginx.list
+echo "deb http://nginx.org/packages/debian/ buster nginx" > /etc/apt/sources.list.d/nginx.list
+echo "deb-src http://nginx.org/packages/debian/ buster nginx" >> /etc/apt/sources.list.d/nginx.list
 apt-get update
 apt-get install -y nginx
+sed -i -e 's/        keepalive_timeout 65;/        keepalive_timeout 2;/g' /etc/nginx/nginx.conf
 systemctl start nginx
